21909610ac01412a830f6e861f97ee876b97ac79
Julien Diot <juliendiot42@gmail.com>
2018-06-16 11:15:13 +0200

    add "R instalation" folder and "installPackages" file

---
+++ R_installation/README.md
@@ -0,0 +1,2 @@
+<!-- pandoc README.md -f commonmark -t html -s -o README.html -->
+This repository contains the files I use for install R on a new computer.
---
+++ R_installation/installPackages.R
@@ -0,0 +1,49 @@
+# My personal file to install usefull pakages
+# Julien Diot
+
+
+# devtools
+install.packages("devtools")
+install.packages("tidyverse")
+
+
+# Rmd ----
+install.packages("rmarkdown")
+install.packages("caTools")
+install.packages("bitops")
+install.packages("tinytex")
+
+
+
+# Shiny ----
+install.packages("shiny")
+install.packages("shinydashboard")
+install.packages("shinyjs")
+install.packages("DT")
+
+
+# graphic library ----
+install.packages("ggplot2")
+install.packages("plotly")
+
+
+
+# Stat ----
+install.packages("MASS")
+
+
+
+# R studio addins ----
+install.packages("addinslist")
+install.packages("colourpicker")
+devtools::install_github("jennybc/jadd") # assign default values
+install.packages("ggedit") # Reproducible layer, scale and theme editing for ggplot2
+
+# Data base ----
+install.packages("RSQLite")
+
+
+
+
+
+
