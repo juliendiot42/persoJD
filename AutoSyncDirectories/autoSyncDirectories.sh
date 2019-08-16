dd0ad5c16321a2f0ee40e7fe11ff8c42b4fd08c9
Julien Diot <juliendiot@ut-biomet.org>
2019-08-16 10:24:39 +0900

    AutoSyncDirectories add variables

--- AutoSyncDirectories/autoSyncDirectories.sh
+++ AutoSyncDirectories/autoSyncDirectories.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+SOURCE="/path/to/source/"
+DEST="/path/to/destination/"
 # do not forget the "/" at the end of the paths source and destination
-while inotifywait -r -e modify,create,delete /path/to/source/; do
-    rsync -vvha --no-p --omit-dir-times --max-size=100m --exclude-from='/path/to/excludedFiles.txt' --delete --delete-excluded /path/to/destination/
+
+while inotifywait -r -e modify,create,delete $SOURCE; do
+    rsync -vvha --no-p --omit-dir-times --max-size=100m --exclude-from='/path/to/excludedFiles.txt' --delete --delete-excluded $SOURCE $DEST
 done
